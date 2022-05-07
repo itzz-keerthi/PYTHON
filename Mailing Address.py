bf0eb190527c785c7a39023ed396a6984073134f
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-05-07 21:32:43 +0530

    Mailing Address

---
+++ Mailing Address.py
@@ -0,0 +1,18 @@
+'''TASK
+Create a program that displays the name and complete mailing address as given in sample outputs.  Your program does not need to read any input from the user.
+
+
+
+Sample Output:
+
+B.Bhuvaneswaran
+
+Department of Computer Science and Engineering
+
+Rajalakshmi Engineering College
+
+Thandalam
+
+Chennai 602 105'''
+
+print("B.Bhuvaneswaran\nDepartment of Computer Science and Engineering\nRajalakshmi Engineering College\nThandalam\nChennai 602 105")
