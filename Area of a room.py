d9e5eebc311dfa32a544f67c4fd4915b36352caa
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-05-07 21:34:24 +0530

    Area of a room

---
+++ Area of a room.py
@@ -0,0 +1,16 @@
+'''TASK
+Write a program that asks the user to enter the width and length of a room. Once these values have been read, your program should compute and display the area of the room. The length and the width will be entered as floating-point numbers.
+
+Sample Input
+
+11.11
+
+22.22
+
+Sample Output
+
+246.86419999999998 square feet '''
+
+width=float(input())
+length=float(input())
+print(width*length,"square feet")
